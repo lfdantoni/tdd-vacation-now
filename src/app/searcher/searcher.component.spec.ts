4d840cea8c4dcf25ab490fbe1e9cc8b5a247d856
lfdantoni <lfdantoni@gmail.com>
2020-02-04 20:20:36 -0300

    searcher tests

--- src/app/searcher/searcher.component.spec.ts
+++ src/app/searcher/searcher.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearcherComponent } from './searcher.component';
+import { By } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 describe('SearcherComponent', () => {
   let component: SearcherComponent;
@@ -8,7 +10,10 @@ describe('SearcherComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SearcherComponent ]
+      declarations: [ SearcherComponent ],
+      imports: [
+        FormsModule
+      ]
     })
     .compileComponents();
   }));
@@ -21,5 +26,31 @@ describe('SearcherComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a search input', () => {
+    const input = fixture.debugElement.query(By.css('input[name=search]'));
+
+    expect(input.nativeElement).toBeDefined();
+  });
+
+  it('should have a search button', () => {
+    const button = fixture.debugElement.query(By.css('button.search-button'));
+
+    expect(button.nativeElement).toBeDefined();
+    expect(button.nativeElement.innerText).toBe('Search Vacation');
+  });
+
+  it('should show results after searching successfully', () => {
+    component.inputSearch = 'New York';
+    const button = fixture.debugElement.query(By.css('button.search-button'));
+    spyOn(component, 'onSearch').and.callThrough();
+
+    button.nativeElement.click();
+    fixture.detectChanges();
+    const results = fixture.debugElement.queryAll(By.css('.hotel-result'));
+    
+    expect(results.length).toBe(component.hotels.length);
+    expect(component.onSearch).toHaveBeenCalled();
   });
 });
--- src/app/searcher/searcher.component.ts
+++ src/app/searcher/searcher.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./searcher.component.scss']
 })
 export class SearcherComponent implements OnInit {
+  inputSearch: string;
+  hotels: string[];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSearch() {
+    this.hotels = ["hotel1", "hotel2", "hotel3"];
+  }
 }
