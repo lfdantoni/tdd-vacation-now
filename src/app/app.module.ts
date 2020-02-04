e4962a0dd3d4a0c070c0df0dfd38fedfe831332d
lfdantoni <lfdantoni@gmail.com>
2020-02-04 19:01:13 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SearchComponent } from './search/search.component';
-import { HotelResultComponent } from './hotel-result/hotel-result.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SearchComponent,
-    HotelResultComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
